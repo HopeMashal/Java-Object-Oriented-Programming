06448d5f313d36e663172d737572330569f6058f
HopeMashal <mashal.hope@gmail.com>
2022-07-19 18:09:21 +0300

    edit lesson 1

--- Notes/lesson_1.java
+++ Notes/lesson_1.java
@@ -20,3 +20,21 @@ public class lesson_1 {
 //? Programming paradigm doesn't refer to a specific language but rather to a way to build a program or a methodology to apply.
 
 /*_______________________________*/
+
+//! Example of previous programming paradigm
+
+//? Procedural Programming: also known as inline programming takes a top-down approach. It is about writing a list of instructions to tell the computer what to do step by step. It relies on procedures or routines.
+
+//? Procedural Programming Example: Program to Calculate Average of array items
+
+// TODO 1: Call to function to accept array data --> Get_input()
+// TODO 2: Call to function to caluclate average --> calc_Avg() --> call calc_Sum()
+
+
+/*_______________________________*/
+
+//! OBJECT:
+
+//? Object is a thing (Tangible - Intangible)
+
+/*_______________________________*/
