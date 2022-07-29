5e3265ded58ef347d553ae130ce4d99b6eef6330
HopeMashal <mashal.hope@gmail.com>
2022-07-29 16:31:07 +0300

    add if statement to rectangle file

--- Notes/lesson_2/Rectangle.java
+++ Notes/lesson_2/Rectangle.java
@@ -4,14 +4,20 @@ public class Rectangle {
   private double length;
   private double width;
   public Rectangle(double length,double width){
-    this.width=width;
-    this.length=length;
+    if(width>0 && length>0){
+      this.width=width;
+      this.length=length;
+    } else {
+      System.out.println("Width and Length must be positive number!!");
+    }
   }
   public void setLength(final double newLength){
-    this.length=newLength;
+    if(newLength>0) this.length=newLength;
+    else System.out.println("Length must be positive number!!");
   }
   public void setWidth(final double newWidth){
-    this.width=newWidth;
+    if(newWidth>0) this.width=newWidth;
+    else System.out.println("Width must be positive number!!");
   }
   public double getLength(){
     return this.length;
