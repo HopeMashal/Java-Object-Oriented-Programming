8512bafc6a17ded5a4c03a97b11c764814dc0449
HopeMashal <mashal.hope@gmail.com>
2022-08-02 14:57:02 +0300

    edit lesson 3 file

--- Notes/lesson_3/lesson_3.java
+++ Notes/lesson_3/lesson_3.java
@@ -8,3 +8,19 @@ public class lesson_3 {
 }
 
 /*_______________________________*/
+
+//! Constructors:
+
+//? Classes can have special methods called constructors
+//? A constructor is a method that is automatically called when an object is created.
+//? Constructors are used to perform operations at the time an object is created.
+//? Constructors typically initialize instance fields and perform other object initialization tasks.
+
+/*
+  * Constructors have the same name as the class
+  * Constructors have no return type (not even void)
+  * Constructors may not return any values
+  * Constructors are typically public
+*/
+
+/*_______________________________*/
