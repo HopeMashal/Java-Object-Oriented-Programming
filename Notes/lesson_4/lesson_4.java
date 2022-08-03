b2105ffaf7f75ab3ed3fc6995ee0eb434758024a
HopeMashal <mashal.hope@gmail.com>
2022-08-03 20:52:55 +0300

    edit lesson 4 file

--- Notes/lesson_4/lesson_4.java
+++ Notes/lesson_4/lesson_4.java
@@ -10,3 +10,11 @@ public class lesson_4 {
 }
 
 /*_______________________________*/
+
+//! Static Method:
+
+//? Static fields and static methods do not belong to a single instance of a class.
+//? To invoke a static method or use a static field, the class name, rather than the instance name, is used.
+//? Ex. double num = Math.sqrt(3.0);
+
+/*_______________________________*/
