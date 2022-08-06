9a67423cc962d6740d166838d185b2c8c136a748
HopeMashal <mashal.hope@gmail.com>
2022-08-06 21:53:55 +0300

    edit lesson 7 files

--- Notes/lesson_7/lesson_7.java
+++ Notes/lesson_7/lesson_7.java
@@ -7,6 +7,11 @@ public class lesson_7 {
   public static void main(String[] args) {
     Student std1 = new Student("Hope", 23, "Jerusalem", "Palestinian", 3, "Engineering", 3.2);
     std1.printInfo();
+    System.out.println(std1.getName());
+    Employee empl1= new Employee("Yuki", 26, "Yafo", "Palestinian", 2000, "team leader", "developer");
+    empl1.printInfo();
+    Employee empl2 = new SalariedEmployee("Akira", 22, "Tokyo", "Japanese", 4000, "leader", "Software", 200, "Full time");
+    empl2.printInfo();
   }
 }
 
