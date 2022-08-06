07ba796ed0541a9160768cb1b277a70997f8481e
HopeMashal <mashal.hope@gmail.com>
2022-08-06 19:53:19 +0300

    edit lesson 6 file

--- Notes/lesson_6/lesson_6.java
+++ Notes/lesson_6/lesson_6.java
@@ -9,7 +9,23 @@ public class lesson_6 {
     Room r2= new Room(10, 50);
     if(r1.isEqual(r2)) System.out.println("The objects are equal");
     else System.out.println("The objects are different");
+    Room r3 = r2; // r3 and r2 are the same object
+    if(r2==r3) System.out.println("The objects are equal");
+    else System.out.println("The objects are different");
+    System.out.println(r2.getLength());
+    r3.setLength(30);
+    System.out.println(r2.getLength());
   }
 }
+
+/*_______________________________*/
+
+//! Methods that copy objects:
+
+//? There are two ways to copy an object:
+
+//? Reference only copy, this is simply copying the address of an object into another reference variable --> Ex Room r3=r2;
+
+//? Deep Copy (Correct), this involves creating a new instance of the class and copying the values from one object into the new object
 
 /*_______________________________*/
