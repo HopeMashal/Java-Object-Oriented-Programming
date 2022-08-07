bef487210b2eae2b1b77c946c9c0d98560f09ad1
HopeMashal <mashal.hope@gmail.com>
2022-08-07 20:31:09 +0300

    edit lesson 11 file

---
+++ Notes/lesson_11/HourlyEmployee.java
@@ -0,0 +1,36 @@
+/*_______________________________*/
+//! Hourly Employee Class!! 
+
+package Notes.lesson_11;
+
+public class HourlyEmployee extends Employee {
+  private double Working_Hours,Hours_Rate;
+  public HourlyEmployee(String name, int age, String address, String nationality,double salary,String rank, String job,double hours,double rate){
+    super(name,age,address,nationality,salary,rank,job);
+    Working_Hours=hours;
+    Hours_Rate=rate;
+  }
+  public void printInfo(){
+    super.printInfo();
+    System.out.println("The employee's working hours is "+Working_Hours+". the employee's hours rate is "+Hours_Rate);
+  }
+  @Override
+  public double getSalary() {
+    return Working_Hours*Hours_Rate;
+  }
+  public double getWorking_Hours() {
+    return Working_Hours;
+  }
+  public void setWorking_Hours(double working_Hours) {
+    Working_Hours = working_Hours;
+  }
+  public double getHours_Rate() {
+    return Hours_Rate;
+  }
+  public void setHours_Rate(double hours_Rate) {
+    Hours_Rate = hours_Rate;
+  }
+  
+}
+
+/*_______________________________*/
--- Notes/lesson_11/lesson_11.java
+++ Notes/lesson_11/lesson_11.java
@@ -4,7 +4,23 @@
 package Notes.lesson_11;
 
 public class lesson_11 {
-  
+  public static void main(String[] args) {
+    
+  }
 }
+
+/*_______________________________*/
+
+//! Interface
+
+//? An interface is similar to an abstract class that has all abstract methods (It can't be instantiated, and all of the methods listed in an interface must be written eleswhere)
+//? The purpose of an interface is to specify behavior for other classes
+//? It is often said that an interface is like a "contract", and when a class implements an interface it must adhere to the contract
+//? The general format of an interface definition:
+/* 
+ * public interface InterfaceName{
+ * (Method headers)
+ * }
+ */
 
 /*_______________________________*/
