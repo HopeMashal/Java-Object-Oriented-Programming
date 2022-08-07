b3b9e6e0b8d3b0bd28d5584fc10c96e34c2c1327
HopeMashal <mashal.hope@gmail.com>
2022-08-07 18:03:39 +0300

    edit lesson 8 file

--- Notes/lesson_8/lesson_8.java
+++ Notes/lesson_8/lesson_8.java
@@ -10,3 +10,14 @@ public class lesson_8 {
 }
 
 /*_______________________________*/
+
+//! Overriding SuperClass Methods:
+
+//? A subclass may have a method with the same signature as a superclass method.
+//? The subclass method overrides the superclass method.
+//? This is known as method overriding
+//? A subclass method that overrides a superclass method must have the same signature as the superclass method
+//? An object of the subclass invokes the subclass's version of the method, not the superclass's
+//? The @Override annotation should be used just before the subclass method
+
+/*_______________________________*/
