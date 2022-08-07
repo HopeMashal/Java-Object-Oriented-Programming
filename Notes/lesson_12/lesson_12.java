fcd04b96478e51d66bec191c223d7edc09f16f4f
HopeMashal <mashal.hope@gmail.com>
2022-08-07 21:33:35 +0300

    edit lesson 12 file

--- Notes/lesson_12/lesson_12.java
+++ Notes/lesson_12/lesson_12.java
@@ -8,3 +8,27 @@ public class lesson_12 {
 }
 
 /*_______________________________*/
+
+//! Enumerated Types
+
+//? Known as an enum, requires declaration and definition like a class
+//? Syntax:
+/* 
+ * enum typeName{ one or more enum constants}
+ */
+//? Definition:
+/* 
+ * enum Day{Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday}
+ * enum CarColor{Red,Black,Silver,Blue}
+ * enum CarType{Porsche,Ferrari,Jaguar}
+ */
+//? Declaration:
+/* 
+ * Day WorkDay;
+ */
+//? Assignment:
+/* 
+ * Day WorkDay = Day.Wednesday;
+ */
+
+/*_______________________________*/
