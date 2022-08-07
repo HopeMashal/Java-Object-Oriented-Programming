82d1465670b5c373863fc9dc66ef20a6b669aee4
HopeMashal <mashal.hope@gmail.com>
2022-08-07 19:49:53 +0300

    edit lesson 10 file

--- Notes/lesson_10/lesson_10.java
+++ Notes/lesson_10/lesson_10.java
@@ -4,7 +4,9 @@
 package Notes.lesson_10;
 
 public class lesson_10 {
-  
+  public static void main(String[] args) {
+    
+  }
 }
 
 /*_______________________________*/
@@ -17,5 +19,18 @@ public class lesson_10 {
 //? A class becomes abstract when you place the abstract key word in the class definition
 
 //? public abstract class ClassName
+
+/*_______________________________*/
+
+//! Abstract Methods
+
+//? An abstract method is a method that appears in a superclass, but expects to be overridden in a subclass
+//? An abstract method has no body and must be overridden in a subclass
+//? AccessSpecifier abstract ReturnType MethodName(PArameterList);
+//? Ex: public abstract void GetSalary();
+
+//? Any class that contains an abstract method is automatically abstract
+//? Abstract methods are used to ensure that a subclass implements the method
+//? If a subclass fails to override an abstract method, a compiler error will result
 
 /*_______________________________*/
