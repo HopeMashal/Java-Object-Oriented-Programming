02fd3b90df7725921b732e9f31a265b15301fe76
HopeMashal <mashal.hope@gmail.com>
2022-08-08 14:01:01 +0300

    edit lesson 13 file

--- Notes/lesson_13/lesson_13.java
+++ Notes/lesson_13/lesson_13.java
@@ -4,7 +4,27 @@
 package Notes.lesson_13;
 
 public class lesson_13 {
-  
+  public static void main(String[] args) {
+    int x=10,y=10;
+    System.out.println(x/y);
+    y=0;
+    System.out.println(x/y);
+  }
 }
+
+/*_______________________________*/
+
+//! Handling Exception
+
+//? An exception is an object that is generated as the result of an error or an unexpected event
+//? Exception are said to have been "thrown"
+//? It is the programmers responsibility to write code that detects and handles exceptions
+//? Unhandled exceptions will crash a program
+//? Java allows you to create exception handlers
+
+/* 
+ * int x=10,y=10;
+ * System.out.println(x/y);
+ */
 
 /*_______________________________*/
