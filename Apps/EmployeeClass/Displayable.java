43fef0e00134c6530fbea27fd2b90eb064cb8193
HopeMashal <mashal.hope@gmail.com>
2022-08-08 17:46:31 +0300

    edit employeeClass files

--- Apps/EmployeeClass/Displayable.java
+++ Apps/EmployeeClass/Displayable.java
@@ -1,5 +1,10 @@
 package Apps.EmployeeClass;
 
 public interface Displayable {
+  String CompanyName="Hope Company", Fax="02-321-4321";
+
+  public void DisplayAllDetails();
+
+  public void DisplayEarning();
   
 }
--- Apps/EmployeeClass/Employee.java
+++ Apps/EmployeeClass/Employee.java
@@ -3,8 +3,56 @@
 
 package Apps.EmployeeClass;
 
+enum GENDER{Male,Female};
 public abstract class Employee {
+  private String Name,Address;
+  private int SSN;
+  private GENDER Gender;
   
+  public Employee() {
+  }
+
+  public Employee(String name, String address, int sSN, GENDER gender) {
+    Name = name;
+    Address = address;
+    SSN = sSN;
+    Gender = gender;
+  }
+
+  public String getName() {
+    return Name;
+  }
+
+  public void setName(String name) {
+    Name = name;
+  }
+
+  public String getAddress() {
+    return Address;
+  }
+
+  public void setAddress(String address) {
+    Address = address;
+  }
+
+  public int getSSN() {
+    return SSN;
+  }
+
+  public void setSSN(int sSN) {
+    SSN = sSN;
+  }
+
+  public GENDER getGender() {
+    return Gender;
+  }
+
+  public void setGender(GENDER gender) {
+    Gender = gender;
+  }
+
+  public abstract double Earning();
+
 }
 
 /*_______________________________*/
